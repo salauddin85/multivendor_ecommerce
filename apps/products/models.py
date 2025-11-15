c7528269ccd4e3e846b7d3af158ad45ba7dfc26b
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-11-15 23:47:58 +0600

    added stores model

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,73 @@
-from django.db import models
+# # apps/products/models.py
 
-# Create your models here.
+# from django.db import models
+# from django.utils.text import slugify
+# from apps.catalog.models import Category, Brand
+# from stores.models import Store
+
+
+# class Product(models.Model):
+#     TYPE = (("simple", "Simple"), ("variable", "Variable"))
+#     STATUS = (
+#         ("draft", "Draft"),
+#         ("pending", "Pending Approval"),
+#         ("published", "Published"),
+#         ("rejected", "Rejected"),
+#     )
+
+#     store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name="products")
+#     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+#     brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, blank=True)
+#     title = models.CharField(max_length=500)
+#     slug = models.SlugField(unique=True)
+#     type = models.CharField(max_length=20, choices=TYPE, default="simple")
+#     description = models.TextField(blank=True)
+#     base_price = models.DecimalField(max_digits=10, decimal_places=2)
+#     main_image = models.CharField(max_length=500, null=True, blank=True)
+#     gallery = models.JSONField(default=list)
+#     stock = models.IntegerField(default=0)
+#     status = models.CharField(max_length=20, choices=STATUS, default="draft")
+#     is_featured = models.BooleanField(default=False)
+#     view_count = models.IntegerField(default=0)
+#     avg_rating = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+
+#     def save(self, *args, **kwargs):
+#         if not self.slug:
+#             self.slug = slugify(self.title)
+#         super().save(*args, **kwargs)
+
+
+# class ProductAttribute(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="attributes")
+#     name = models.CharField(max_length=100)
+#     is_variation = models.BooleanField(default=True)
+
+    
+
+
+# class ProductAttributeValue(models.Model):
+#     attribute = models.ForeignKey(ProductAttribute, on_delete=models.CASCADE, related_name="values")
+#     value = models.CharField(max_length=100)
+#     color_code = models.CharField(max_length=20, null=True, blank=True)
+
+    
+
+# class ProductVariant(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="variants")
+#     sku = models.CharField(max_length=50, unique=True)
+#     variant_name = models.CharField(max_length=255)
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+#     stock = models.IntegerField(default=0)
+#     image = models.CharField(max_length=500, blank=True, null=True)
+
+
+# class ProductVariantAttribute(models.Model):
+#     variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, related_name="variant_attrs")
+#     attribute = models.ForeignKey(ProductAttribute, on_delete=models.CASCADE)
+#     value = models.ForeignKey(ProductAttributeValue, on_delete=models.CASCADE)
+
+#     class Meta:
+#         unique_together = ("variant", "attribute")
--- apps/stores/models.py
+++ apps/stores/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+from apps.authentication.models import StoreOwner,Vendor
+from .constants.store_choices import TYPE_CHOICES, STATUS_CHOICES
 
-# Create your models here.
+
+
+class StoreBaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        
+
+class CommissionRate(models.Model):
+    store_type = models.CharField(max_length=20, choices=TYPE_CHOICES)  # vendor / company
+    rate = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Store(StoreBaseModel):
+    """Store/Shop model for vendors and companies"""
+    
+    store_owner = models.ForeignKey(StoreOwner, on_delete=models.CASCADE, related_name='stores',null=True,blank=True)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='vendors',null=True,blank=True)
+    store_name = models.CharField(max_length=255, unique=True,default='')
+    slug = models.SlugField(max_length=255, unique=True)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES,default='company')
+    logo = models.ImageField(upload_to='store_logos/', blank=True, null=True)
+    banner = models.ImageField(upload_to='store_banners/', blank=True, null=True)
+    address = models.CharField(max_length=255,default='')
+    description = models.TextField(default='')
+    commission_rate = models.ForeignKey(CommissionRate, on_delete=models.SET_NULL, null=True, blank=True)
+    is_verified = models.BooleanField(default=False)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.store_name)
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.store_name
+
+
+
+class StoreAnalytics(StoreBaseModel):
+    store = models.OneToOneField(
+        Store, 
+        on_delete=models.CASCADE, 
+        related_name='analytics'
+    )
+
+    # Views / Traffic
+    views_count = models.PositiveIntegerField(default=0)
+
+    # Product stats
+    products_count = models.PositiveIntegerField(default=0)
+
+    # Sales stats
+    orders_count = models.PositiveIntegerField(default=0)
+    total_sold_quantity = models.PositiveIntegerField(default=0)
+    total_revenue = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+
+    # Ratings
+    average_rating = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+    total_reviews = models.PositiveIntegerField(default=0)
+
+    # Followers (optional)
+    followers_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"Analytics for {self.store.store_name}"
